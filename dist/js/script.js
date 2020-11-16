23d086534c05c2a565778691d21f08d9f36f7e17
varekaiwork <serhiisaviukwork@gmail.com>
2020-11-16 15:41:49 +0200

    init

---
+++ dist/js/script.js
@@ -0,0 +1,36 @@
+
+document.addEventListener('DOMContentLoaded', function(){ 
+  const open = document.querySelector("#open_popup")
+  const popupBg = document.querySelector('.popup-bg')
+  const close = document.querySelector('#close')
+  const content =document.querySelector('.content')
+
+ 
+
+
+  function closed(){
+    popupBg.classList.remove("visible")
+    content.classList.remove('overflow')
+  }
+  open.addEventListener('click', function(){
+    popupBg.classList.add("visible")
+    content.classList.add('overflow')
+   
+  })
+  close.addEventListener('click', closed)
+  popupBg.addEventListener('click', (e)=>{
+    if(e.target===popupBg || e.keyCode === 27 ){
+      closed()
+    }
+  
+  })
+  
+  document.addEventListener('keydown',(e)=>{
+    if(e.code ==="Escape"){
+      closed()
+    }
+  })
+  
+})
+
+
